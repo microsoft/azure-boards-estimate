07d46d7ceed0f3d143d5fc7dcd345eebd5f5df7d
AminTi <54841342+AminTi@users.noreply.github.com>
2021-12-06 12:40:45 +0000

    5000temas in dropdown

--- src/services/teams.ts
+++ src/services/teams.ts
@@ -16,14 +16,7 @@ export interface IIteration {
    
 }
 
-export interface  Nawal {
-    id: string;
-    name: string;
-    description: string;
-    projectId: string;
-    projectName: string;
-    url: string;
-}
+
 
 
 export interface ITeamService extends IService {
@@ -36,29 +29,30 @@ export interface ITeamService extends IService {
 export const TeamServiceId = "TeamService";
 
 export class TeamService implements ITeamService {
-public async getAllTeams(projectId: string): Promise<ITeam[]> {
-    const client = getClient(CoreRestClient);
-     const LIMIT = 5000;
-    let skip = 0
-
-    const gettingAllTeams=  async () => {
-        const allData = []
-        for (let i = 0; i < LIMIT; i++) {
-         const teams = await client.getTeams(projectId, false, 1000, skip);
-          allData.push(teams)
-          skip += 1000
-          if( skip === LIMIT || !teams.length)  
-          return allData.flat()
+    public async getAllTeams(projectId: string): Promise<ITeam[]> {
+        const client = getClient(CoreRestClient);
+         const LIMIT = 5000;
+        let skip = 0
+        const gettingAllTeams = async () =>{
+            const allData : any = []
+            for (let i = 0; i < LIMIT; i++) {
+             const teams = await client.getTeams(projectId, false, 1000, skip);
+              allData.push(teams)
+              skip += 1000
+              if( skip === LIMIT || !teams.length)  
+              return allData.flat()
+            }
         }
+        const data = await gettingAllTeams()
+         const allTeams = data.map(({ id, name }:ITeam) => ({
+            id,
+            name
+        }));
+        allTeams.sort((a :any, b:any) => a.name.localeCompare(b.name));
+        console.log(allTeams)
+        return allTeams;
     }
-    const data:any = await gettingAllTeams()
-    const allTeams = data && data.map(({ id, name}:ITeam) => ({
-        id,
-        name
-    }));
-    allTeams.sort((a:any, b:any) => a.name.localeCompare(b.name));
-   return allTeams;
-}
+
 
   public async getIterationsForTeam(teamId: string): Promise<IIteration[]> {
         const projectService: IProjectPageService = await DevOps.getService<
