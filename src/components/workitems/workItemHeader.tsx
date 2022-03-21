c5ea6d6d98a0ff3a51e2a64c8a17422e3b901b41
AminTi <54841342+AminTi@users.noreply.github.com>
2022-03-21 12:49:55 +0000

    create custon link and refactor to functional cpmnt

--- src/components/workitems/workItemHeader.tsx
+++ src/components/workitems/workItemHeader.tsx
@@ -1,59 +1,62 @@
-import { ILocationService } from "azure-devops-extension-api";
+
+import * as React from "react";
 import { IWorkItemFormNavigationService } from "azure-devops-extension-api/WorkItemTracking";
 import * as DevOps from "azure-devops-extension-sdk";
-import * as React from "react";
 import { IWorkItem } from "../../model/workitem";
 import { WorkItemTypeIcon } from "./typeIcon";
 import "./workItemHeader.scss";
 
+
+
 export interface IWorkItemHeaderProps {
     workItem: IWorkItem;
+
 }
+export const WorkItemHeader: React.FC<IWorkItemHeaderProps> = (props) => {
 
-type MyState = { url: string };
-export class WorkItemHeader extends React.Component<IWorkItemHeaderProps, MyState > {
-    private workItemUrl = "#";
-    constructor(props:any) {
-        super(props);
-        this.state = {url: ""};
-      }
+    const { workItem: { id, project, title, workItemType, icon, color, } } = props;
+    const [currentHostName, setCurrentHostName] = React.useState<string>("#")
 
-    async componentDidUpdate() {
-        const locationService = await DevOps.getService<ILocationService>("ms.vss-features.location-service");
-        this.workItemUrl = await locationService.routeUrl(
-            "ms.vss-work-web.work-items-form-route-with-id",
-            { project: this.props.workItem.project, id: this.props.workItem.id.toString() });
-            this.setState({url: this.workItemUrl})
-           }
-    
-       render(): JSX.Element {
-        const {
-            workItem: { id, project, title, workItemType, icon, color }
-        } = this.props;
+     const getCurrentHost = async () => {
+        const locationService = await DevOps.getHost()
+        setCurrentHostName(locationService.name)
+    }
 
-         const openWi = async (ev:any)=>{
-            if(!ev.ctrlKey && !ev.metaKey &&!ev.altKey && !ev.shiftKey){
+    React.useEffect(() => {
+        getCurrentHost()
+    }, [])
+
+   
+    const openWi = async (ev: any) => {
+        if (!ev.ctrlKey && !ev.metaKey && !ev.altKey && !ev.shiftKey) {
             ev.preventDefault();
             const service = await DevOps.getService<IWorkItemFormNavigationService>("ms.vss-work-web.work-item-form-navigation-service");
             service.openWorkItem(id);
         }
-      }
-        return (
+    }
+
+    let currentCustomUrl = `https://dev.azure.com/${currentHostName}/${project}/_workitems/edit/${id}/`
+
+     return (
+        <div>
             <div className="work-item-header">
                 <div className="work-item-header--header">
                     <a
-                       className="work-item-header--info"
-                        href={this.state.url}
+                        className="work-item-header--info"
+                        href={currentCustomUrl ? currentCustomUrl : "#"}
                         target="_blank"
-                    
-                        onClick={(ev)=> openWi(ev)}
+
                     >
-                      <WorkItemTypeIcon icon={icon} color={color} />
-                        {workItemType} {id}
+                        <div onClick={(ev) => openWi(ev)}>
+                            <WorkItemTypeIcon icon={icon} color={color} />
+                            {workItemType} {id}
+                        </div>
                     </a>
                     <div className="work-item-header--title">{title}</div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    )
 }
+
+
