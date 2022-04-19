cb6bd8bca605d6387234123833c8009f7d47b75d
AminTi <54841342+AminTi@users.noreply.github.com>
2022-04-19 13:44:19 +0200

    create custom url link and refactor to functional cpmnt. issues#106 (#107)

--- src/components/workitems/workItemHeader.tsx
+++ src/components/workitems/workItemHeader.tsx
@@ -1,62 +1,63 @@
-import { ILocationService } from "azure-devops-extension-api";
-import { IWorkItemFormNavigationService } from "azure-devops-extension-api/WorkItemTracking";
-import * as DevOps from "azure-devops-extension-sdk";
+
 import * as React from "react";
+import { IWorkItemFormNavigationService} from "azure-devops-extension-api/WorkItemTracking";
+import * as DevOps from "azure-devops-extension-sdk";
 import { IWorkItem } from "../../model/workitem";
 import { WorkItemTypeIcon } from "./typeIcon";
 import "./workItemHeader.scss";
+import { ILocationService } from "azure-devops-extension-api";
+
+
+
 
 export interface IWorkItemHeaderProps {
     workItem: IWorkItem;
+
 }
+export const WorkItemHeader: React.FC<IWorkItemHeaderProps> =  (props) => {
 
-type MyState = { url: string };
-export class WorkItemHeader extends React.Component<IWorkItemHeaderProps, MyState > {
-    private workItemUrl = "#";
-    constructor(props:any) {
-        super(props);
-        this.state = {url: ""};
-      }
+    const { workItem: { id, project, title, workItemType, icon, color, } } = props;
+    const [currentUrl ,setCurrentUrl] = React.useState<string>("#")
+    
+   React.useEffect(() => {
+    getBaseURl()
+     },[])
 
-    async clickHandler() {
-        const locationService = await DevOps.getService<ILocationService>("ms.vss-features.location-service");
-        this.workItemUrl = await locationService.routeUrl(
-            "ms.vss-work-web.work-items-form-route-with-id",
-            { project: this.props.workItem.project, id: this.props.workItem.id.toString() });
-            this.setState({url: this.workItemUrl})
-        
-           }
-
-       render(): JSX.Element {
-        const {
-            workItem: { id, project, title, workItemType, icon, color }
-        } = this.props;
-
-       const openWi = async (ev:any)=>{
-            this.clickHandler()
-            if(!ev.ctrlKey && !ev.metaKey &&!ev.altKey && !ev.shiftKey){
+   
+    const openWi = async (ev: any) => {
+      
+        if (!ev.ctrlKey && !ev.metaKey && !ev.altKey && !ev.shiftKey) {
             ev.preventDefault();
             const service = await DevOps.getService<IWorkItemFormNavigationService>("ms.vss-work-web.work-item-form-navigation-service");
             service.openWorkItem(id);
-        }
-      }
-        return (
-            <div className="work-item-header">
-                <div className="work-item-header--header">
-                    <a
-                       className="work-item-header--info"
-                        href={this.state.url}
-                        target="_blank"
-                    
-                        onClick={(ev)=> openWi(ev)}
-                    >
-                      <WorkItemTypeIcon icon={icon} color={color} />
-                        {workItemType} {id}
-                       
-                    </a>
-                    <div className="work-item-header--title">{title}</div>
-                </div>
-            </div>
-        );
-    }
+        }}
+
+
+        
+ const getBaseURl = async () => {
+    const locationService = await DevOps.getService<ILocationService>("ms.vss-features.location-service");
+    setCurrentUrl(await locationService.getServiceLocation())
+};
+
+let currentCustomUrl = `${currentUrl}/${project}/_workitems/edit/${id}/`
+   return (
+      <div>
+          <div className="work-item-header">
+              <div className="work-item-header--header">
+                  <a
+                      className="work-item-header--info"
+                      href={currentCustomUrl ? currentCustomUrl : "#"}
+                      target="_blank">
+                      <div onClick={(ev) => openWi(ev)}>
+                          <WorkItemTypeIcon icon={icon} color={color} />
+                          {workItemType} {id}
+                      </div>
+                  </a>
+                  <div className="work-item-header--title">{title}</div>
+              </div>
+          </div>
+      </div>
+  )
 }
+
+
