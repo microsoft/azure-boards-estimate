d26dc507e7337b54daf6cb02562a82420612626d
AminTi <54841342+AminTi@users.noreply.github.com>
2022-06-12 09:39:47 +0000

    replace character

--- src/pages/session/components/customEstimate.tsx
+++ src/pages/session/components/customEstimate.tsx
@@ -3,34 +3,25 @@ import { Button } from "azure-devops-ui/Button";
 import "./customEstimate.scss"
 
 export const CustomEstimate: React.FC<{
-    commitEstimate: (value: string |null ) => void;
+    commitEstimate: (value: string | null) => void;
 }> = props => {
     const [value, setValue] = React.useState<string | null>(null);
 
 
- 
-  
-    const replaceChar = ()=>{
-       if(value &&  value.includes(".")){
-    let currentVal = value   
-     return  currentVal.replace(".", ",")
-       }
-       return value 
+    const replaceChar = () => {
+        if (value && value.includes(".")) {
+            let currentVal = value
+            return currentVal.replace(".", ",")
+        }
+        return value
     }
-  
-  
 
-    return (
+return (
         <div className="flex-row">
-          <input
-             className="custom-values-input"
-   
-             onChange={(e) => setValue(e.target.value)}
-             type="text"
-
-               
-             
-            />
+            <input
+                className="custom-values-input"
+                onChange={(e) => setValue(e.target.value)}
+                type="text" />
             <Button
                 className="custom-values-input"
                 onClick={() => {
@@ -42,6 +33,6 @@ export const CustomEstimate: React.FC<{
     );
 };
 
-  
+
 
 
